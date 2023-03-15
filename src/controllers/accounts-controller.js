812073a0e1e26fc3a778f87225cca46d6af6927e
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-03-15 15:27:16 +0000

    cookie clear upon user logout, adding .env file security

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -45,6 +45,7 @@ export const accountsController = {
   logout: {
     auth: false,
     handler: function (request, h) {
+      request.cookieAuth.clear();
       return h.redirect("/");
     },
   },
--- src/server.js
+++ src/server.js
@@ -5,9 +5,16 @@ import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import Cookie from "@hapi/cookie";
+import dotenv from "dotenv";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -32,8 +39,8 @@ async function init() {
   await server.register(Cookie);
   server.auth.strategy("session", "cookie", {
     cookie: {
-      name: "playmark",
-      password: "secretpasswordnotrevealedtoanyone",
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
       isSecure: false,
     },
     redirectTo: "/",
